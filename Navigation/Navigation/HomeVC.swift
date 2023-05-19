ffdeab4714a37d7b05387b6921610e128b127158
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-05-19 15:56:06 +0200

    Added routing between pages

---
+++ Navigation/Navigation/HomeVC.swift
@@ -0,0 +1,57 @@
+//
+//  HomeVC.swift
+//  Navigation
+//
+//  Created by DA MAC M1 121 on 2023/05/19.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
