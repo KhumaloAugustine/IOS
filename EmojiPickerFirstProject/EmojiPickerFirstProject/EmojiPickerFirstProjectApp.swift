84272a90a77ea349cc334b6f3db8ff0a52a4647c
KhumaloAugustine <mr.a.s.khumalo@gmail.com>
2023-05-19 12:31:45 +0200

    Adding my IOS First Practice Projects

---
+++ EmojiPickerFirstProject/EmojiPickerFirstProject/EmojiPickerFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPickerFirstProjectApp.swift
+//  EmojiPickerFirstProject
+//
+//  Created by DA MAC M1 121 on 2023/05/19.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPickerFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by DA MAC M1 121 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
